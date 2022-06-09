a1d43931802f1626b09b694c039fdcc1809a9800
Petar <poreskovi@tvz.hr>
2022-06-09 16:23:10 +0200

    commit -m "v2"

---
+++ bodovi.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Racunalko</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="okvir">
+        <div class="header">Racunalko</div>
+        <div class="Zadatak">
+            <h4>Marko Bicko LEVEL: 7 Razred: 3.C</h4>
+            <h1>Zadatak 1</h1>
+            <h2> 64 : 8 = ?</h2>
+            <div class="odgovori">
+                <div>8</div>
+                <div>12</div>
+                <div>7</div>
+                <div>4</div>
+            </div>
+        </div>
+        <div class="potvrda">
+            <h3>Potvrdi odgovor</h3>
+        </div>
+    </div>
+
+    
+</body>
+</html>
